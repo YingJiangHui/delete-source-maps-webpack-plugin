e5e0ba30ffdcf3748874655897845ebd42f563a0
Ying <473380917@qq.com>
2021-06-18 16:08:31 +0800

    init project

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import {Compiler} from 'webpack'
+import path from 'path'
+import fs from 'fs'
+
+class WebpackPluginDeleteSourcemap {
+  apply(compiler:Compiler){
+    compiler.hooks.done.tap('webpack-plugin-delete-source-map',(stats)=>{
+      try{
+        const {compilation} = stats
+        const outputPath = compilation.outputOptions.path
+        const sourcemapFileCount = Object.keys(compilation.assets).filter((filename)=>/[a-zA-Z0-9]\.(js|css)\.map$/.test(filename))
+        .map((filename)=>{
+          if(!outputPath) return
+          const filePath = path.resolve(outputPath,filename)
+          fs.unlinkSync(filePath)
+        }).length
+        console.log(`------ deleted sourcemap files: ${sourcemapFileCount} ------`)
+      }catch(e){
+      
+      }
+
+    })
+  }
+}
+
+export default WebpackPluginDeleteSourcemap
